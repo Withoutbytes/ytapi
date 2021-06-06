5b8a47e594d3beb372f4b8373265411379623630
Jonathan <41968795+Withoutbytes@users.noreply.github.com>
2021-06-06 15:44:44 -0300

    up

---
+++ main.ts
@@ -0,0 +1,62 @@
+import ytdl from "ytdl-core";
+import yts from "yt-search";
+import express from "express";
+const app = express();
+
+app.use(express.query({}))
+
+app.get("/music", async (req, res) => {
+    const { query } = req.query;
+    console.log({ query });
+
+    if (typeof query != "string") return res.status(400).send("Missing video query param");
+    try {
+        const qRes = await yts(query);
+        const videoId = qRes.videos[0].videoId;
+        if (!videoId) return res.status(404).send("video not found");
+        console.log(qRes.videos[0].title);
+        res.setHeader("Content-Type", "video/mp4");
+        ytdl(videoId, {
+            filter: format => format.container === 'mp4',
+            // quality: "140",
+            // filter: 'audioonly',
+            quality: "lowest"
+        }).pipe(res);
+    } catch (e) {
+        const error = e as Error;
+        res.status(500).send(error?.message);
+    }
+});
+
+app.get("/music/play", async (req, res) => {
+    const { videoId } = req.query;
+    if (typeof videoId != "string") return res.status(400).send("Missing video query param");
+    try {
+        const info = await ytdl.getBasicInfo(videoId);
+
+        ytdl(videoId, {
+            filter: format => format.container === 'mp4',
+            // quality: "140",
+        }).pipe(res);
+    } catch (e) {
+        const error = e as Error;
+        res.status(500).send(error?.message);
+    }
+});
+
+app.get("/music/search", async (req, res) => {
+    const { query } = req.query;
+    if (typeof query != "string") return res.status(400).send("Missing video query param");
+    try {
+        const qRes = await yts(query);
+        const video = qRes.videos[0];
+        if (!video) return res.status(404).send("video not found");
+        res.send({ videoId: video.videoId, title: video.title });
+    } catch (e) {
+        const error = e as Error;
+        res.status(500).send(error?.message);
+    }
+})
+
+app.listen(8083, () => console.log("Server at http://localhost:8083"));
+
